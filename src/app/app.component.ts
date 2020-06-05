7f19fe6e6c372fd34a1fa3813279b18a8c41b01e
Ramesh Thiruchelvam <ramesh.thiruchelvam@cambio.lk>
2020-06-05 12:27:05 +0530

    feat: update subject outside angular zone.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,24 @@
-import { Component } from '@angular/core';
-import { interval, Observable } from 'rxjs';
-import { map, startWith } from "rxjs/operators";
+import { Component, NgZone, OnDestroy } from '@angular/core';
+import { interval, Observable, Subject, BehaviorSubject } from 'rxjs';
+import { map, startWith, takeUntil } from "rxjs/operators";
 
 @Component({
   selector: 'app-root',
-  template: `<span id="time">{{time$ | async | date: 'short'}}</span>`
+  template: `<span id="time">{{timeSub | async | date: 'short'}}</span>`
 })
-export class AppComponent {
+export class AppComponent implements OnDestroy {
 
-  time$: Observable<Date>;
+  private readonly destroySub = new Subject<void>();
+  timeSub: BehaviorSubject<Date> = new BehaviorSubject(new Date());
 
-  constructor() {
-    this.time$ = interval(60000).pipe(startWith(new Date()), map(tick => new Date()));
+  constructor(private readonly zone: NgZone) {
+    this.zone.runOutsideAngular(() => {
+      interval(60000).pipe(map(tick => new Date()), takeUntil(this.destroySub)).subscribe(this.timeSub.next)
+    });
   }
 
+  ngOnDestroy() {
+    this.destroySub.next();
+    this.destroySub.complete();
+  }
 }
